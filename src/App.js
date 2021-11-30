ccbf670aaae91f6f4d78f12f9d6088215a9c51b2
lbowles <lspbowles@gmail.com>
2021-11-30 13:03:44 +0200

    added web3js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import SearchBar from './Components/SearchBar';
 import MainStats from './Components/MainStats';
 import RebaseAleart from './Components/RebaseAleart';
 import Footer from './Components/Footer';
-
+import ConnectWallet from './Components/ConnectWallet';
+import Web3 from 'web3'
 import './App.css';
 
 
 function App() {
   const onChange = () => {
-    
+  }
+
+  async function logInWithEth() {
+    if (window.ethereum) {
+    } else {
+      alert("No ETH browser extention detected")
+    }
   }
   return (
     <div className="App">
-      <h1>Klima Incoom</h1>
+      
+      <h1>Klima Incoom </h1>
+      <ConnectWallet />
       <SearchBar onChange={onChange} />
       <MainStats />
       <RebaseAleart />
       <Footer />
+      
     </div>
     
   );
---
+++ src/Components/ConnectWallet.js
@@ -0,0 +1,13 @@
+import './ConnectWallet.css'
+
+const ConnectWallet = () => {
+    return (
+        
+        <div>
+            <button className="metamaskBtn"><h2>Connect Wallet</h2>
+            </button>
+        </div>
+    )
+}
+
+export default ConnectWallet
